644dc8b7b915270b7bcb5f15c167edd7f9bb73a9
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-10-25 18:58:00 +0500

    just API file made

---
+++ Bid/backend/src/controllers/User/myPrdoucts.controller.js
@@ -0,0 +1,17 @@
+import bcrypt from "bcrypt";
+import { validationResult } from "express-validator";
+import Jwt from "jsonwebtoken";
+import Product from "../../models/Product.js";
+import User from "../../models/User.js";
+
+import { statusCode } from "../../utils/constant.js";
+import { jsonGenerate } from "../../utils/helpers.js";
+
+const myProducts = async (req, res) => {
+  const error = validationResult(req);
+
+  if (error.isEmpty()) {
+  }
+};
+
+export default myProducts;
