a27de51d427a23b59e609e51b4383aceeaac8340
Admanii <ahmedriaz-50@hotmail.com>
2022-11-02 00:59:34 +0500

    Login Page Draft

--- BidBuy/frontend/src/components/LogIn/Login.jsx
+++ BidBuy/frontend/src/components/LogIn/Login.jsx
@@ -13,8 +13,11 @@ import {
 
 function Login() {
   return (
-    <div>
-        <h2> Welcome Back</h2>
+    <center>
+   
+    <div className='div_main'>
+        <div className='div_signIn'>
+        <h2 className='welcome_heading'> Welcome Back</h2>
         <h4>  Sign in to your account</h4>
         <div> 
             <TextField id="standard-basic" label="Email Address" variant="standard" />
@@ -22,17 +25,22 @@ function Login() {
             <TextField id="standard-basic" label="Password" variant="standard" />
         </div>
         <Button>Forget Password</Button>
-        <Button className='sign-in' variant="contained">Sign In</Button>
+        <Button className='btn_signIn'>Sign In</Button>
         <div>
             OR
             <br/>
-            <Button className='sign-in' variant="contained">Sign in with Google</Button>
+            <Button className='sign-in'>Sign in with Google</Button>
         </div>
         <div>
             <p>Dont have an account?<Button variant="text" style={{color: 'red'}}>Sign Up</Button></p>  
         </div>
-        <div className='red-container'> Hello</div>
-    </div>
+        </div>
+        <div className='div_redContainer'>
+        <h1 className='newHere_heading'>New Here?</h1>
+        <p className='txt_startSelling'>Sign Up and start selling!</p>
+           </div>
+      </div>
+    </center>
   )
 }
 
