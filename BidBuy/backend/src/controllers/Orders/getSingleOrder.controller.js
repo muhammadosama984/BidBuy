906b0e3ccd2c7bf7cf081fefd92632169888a6cf
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-11-03 23:01:48 +0500

    update myOrders

--- BidBuy/backend/src/controllers/Orders/getSingleOrder.controller.js
+++ BidBuy/backend/src/controllers/Orders/getSingleOrder.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import { validationResult } from "express-validator";
 import Jwt from "jsonwebtoken";
+import OrdersCompleted from "../../models/OrdersCompleted.js";
 import Product from "../../models/Product.js";
 import User from "../../models/User.js";
 
@@ -12,7 +13,28 @@ const getSingleorder = async (req, res) => {
 
   if (error.isEmpty()) {
     try {
-    } catch (error) {}
+      const data = await OrdersCompleted.findById(req.query.id).select([
+        "bidding_id",
+        "seller_id",
+        "buyer_id",
+        "price_soldIn",
+        "payment",
+      ]);
+      if (!data) {
+        return res.json(
+          jsonGenerate(statusCode.SUCCESS, "Order not find", data)
+        );
+      }
+      return res.json(jsonGenerate(statusCode.SUCCESS, "Order Showing", data));
+    } catch (error) {
+      return res.json(
+        jsonGenerate(
+          statusCode.UNPROCESSABLE_ENTITY,
+          "Error is displaying products",
+          error
+        )
+      );
+    }
   }
 };
 
--- BidBuy/backend/src/controllers/User/getMyOrders.controllers.js
+++ BidBuy/backend/src/controllers/User/getMyOrders.controllers.js
@@ -1,7 +1,31 @@
 import bcrypt from "bcrypt";
 import { validationResult } from "express-validator";
 import Jwt from "jsonwebtoken";
+import OrdersCompleted from "../../models/OrdersCompleted.js";
 import Product from "../../models/Product.js";
 import User from "../../models/User.js";
 import { statusCode } from "../../utils/constant.js";
 import { jsonGenerate } from "../../utils/helpers.js";
+
+const myOrders = async (req, res) => {
+  const error = validationResult(req);
+
+  if (error.isEmpty()) {
+    try {
+      const data = await OrdersCompleted.find({ seller_id: req.userId }).select(
+        ["bidding_id", "seller_id", "buyer_id", "price_soldIn", "payment"]
+      );
+      res.json(jsonGenerate(statusCode.SUCCESS, "My Orders", data));
+    } catch (error) {
+      return res.json(
+        jsonGenerate(
+          statusCode.UNPROCESSABLE_ENTITY,
+          "Error is displaying profile",
+          error
+        )
+      );
+    }
+  }
+};
+
+export default myOrders;
