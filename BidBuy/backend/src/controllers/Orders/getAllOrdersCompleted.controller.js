fe02d6256f53cbe7df5fef066776dbcb657bec0b
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-11-03 22:53:42 +0500

    Update getAllOrdersCompleted.js

--- BidBuy/backend/src/controllers/Orders/getAllOrdersCompleted.controller.js
+++ BidBuy/backend/src/controllers/Orders/getAllOrdersCompleted.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import { validationResult } from "express-validator";
 import Jwt from "jsonwebtoken";
+import OrdersCompleted from "../../models/OrdersCompleted.js";
 import Product from "../../models/Product.js";
 import User from "../../models/User.js";
 
@@ -12,8 +13,41 @@ const getAllOrdersCompleted = async (req, res) => {
 
   if (error.isEmpty()) {
     try {
-    } catch (error) {}
+      const list = await OrdersCompleted.find({ active: true }).select([
+        "bidding_id",
+        "seller_id",
+        "buyer_id",
+        "price_soldIn",
+        "payment",
+      ]);
+      const isAdmin = await User.findById(req.userId).select("userRole");
+      //console.log(isAdmin);
+      if (isAdmin.userRole === "User") {
+        return res.json(
+          jsonGenerate(
+            statusCode.SUCCESS,
+            "You are not authorised to delete profile"
+          )
+        );
+      }
+      return res.json(jsonGenerate(statusCode.SUCCESS, "All Orders", list));
+    } catch (error) {
+      return res.json(
+        jsonGenerate(
+          statusCode.UNPROCESSABLE_ENTITY,
+          "Error is displaying Orders",
+          error
+        )
+      );
+    }
   }
+  res.json(
+    jsonGenerate(
+      statusCode.VALIDATION_ERROR,
+      "Validation Error",
+      error.mapped()
+    )
+  );
 };
 
 export default getAllOrdersCompleted;
