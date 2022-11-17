d002f3f839c595ba5a819815da019e2e5853c1f1
Admanii <ahmedriaz-50@hotmail.com>
2022-11-17 15:21:51 +0500

    sfsadf

--- BidBuy/frontend/src/components/MainPage/MainPage.jsx
+++ BidBuy/frontend/src/components/MainPage/MainPage.jsx
@@ -1,26 +1,31 @@
 import { Button } from '@mui/material'
 import React from 'react'
-import { useState , useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { api } from '../../App.jsx'
 import NavBar from '../NavBar/NavBar'
-function MainPage() {
-    const [products, setproducts] = useState([])
-    let double = [];
-    const getProducts = () => {
-        api.get('/getallproducts').then(res=>{
-          //console.log(res.data.data);
-          setproducts( res.data.data);
+import CardProduct from '../ProductGrid/CardProduct.jsx'
 
-         // console.log(products);
-         
-           
-        })
-    }
-    const postProducts = () => {
-      api.post('/login', {
-        username: "MuhammadOsamaAsif9844", 
-        password: "osama123456"
-      })
+import myImage from '../../images/ip14.jpg'
+import { Stack } from '@mui/system'
+
+function MainPage() {
+  const [products, setproducts] = useState([])
+  let double = [];
+  const getProducts = () => {
+    api.get('/getallproducts').then(res => {
+      //console.log(res.data.data);
+      setproducts(res.data.data);
+
+      // console.log(products);
+
+
+    })
+  }
+  const postProducts = () => {
+    api.post('/login', {
+      username: "MuhammadOsamaAsif9844",
+      password: "osama123456"
+    })
       .then(function (response) {
         console.log(response.data.data.token);
       })
@@ -29,17 +34,39 @@ function MainPage() {
       });
   }
 
-    // useEffect(() => {
-    //  getProducts()
-    // }, [])
-    
+  // useEffect(() => {
+  //  getProducts()
+  // }, [])
+
   return (
     <div>
-      <NavBar/>
-        <Button onClick={getProducts}>Hello</Button>
-        <h3>{products.length === 0? <></>: products[0].name}</h3>
-       
-     
+      <NavBar />
+      <Button onClick={getProducts}>Hello</Button>
+      <h3>{products.length === 0 ? <></> : products[0].name}</h3>
+
+      <Stack direction="column" spacing={0} alignItems={"center"}>
+        <Stack direction="row" spacing={0} alignItems={"center"}>
+          {/* Image Not using props currently*/}
+          <CardProduct image={myImage} />
+          <CardProduct image={myImage} />
+          <CardProduct image={myImage} />
+          <CardProduct image={myImage} />
+        </Stack>
+        <Stack direction="row" spacing={0} alignItems={"center"}>
+          {/* Image Not using props currently*/}
+          <CardProduct image={myImage} />
+          <CardProduct image={myImage} />
+          <CardProduct image={myImage} />
+          <CardProduct image={myImage} />
+        </Stack>
+        <Stack direction="row" spacing={0} alignItems={"center"}>
+          {/* Image Not using props currently*/}
+          <CardProduct image={myImage} />
+          <CardProduct image={myImage} />
+          <CardProduct image={myImage} />
+          <CardProduct image={myImage} />
+        </Stack>
+      </Stack>
     </div>
   )
 }
