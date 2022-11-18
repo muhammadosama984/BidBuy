5c40b34c03cb7fee905e3bf48572d3311012d648
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-11-18 15:28:13 +0500

    Update getAllUsers.js

---
+++ BidBuy/backend/src/controllers/User/getAllUsers.controller.js
@@ -0,0 +1,31 @@
+import bcrypt from "bcrypt";
+import { validationResult } from "express-validator";
+import Jwt from "jsonwebtoken";
+import User from "../../models/User.js";
+import { statusCode } from "../../utils/constant.js";
+import { jsonGenerate } from "../../utils/helpers.js";
+
+export const getAllUsers = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    try {
+      const list = await User.find({ active: true }).select([
+        "name",
+        "username",
+        "email",
+        "date",
+        "password",
+      ]);
+      return res.json(jsonGenerate(statusCode.SUCCESS, "All Users", list));
+    } catch (error) {
+      return res.json(
+        jsonGenerate(
+          statusCode.UNPROCESSABLE_ENTITY,
+          "Error in displaying in User",
+          error
+        )
+      );
+    }
+  }
+};
