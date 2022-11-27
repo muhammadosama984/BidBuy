3e5032652ae170cf8bf7797be43c8da6eb854b45
Muhammad Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-11-28 00:14:57 +0500

    cloudinary trying

--- BidBuy/frontend/src/components/ProductGrid/CardGrid.jsx
+++ BidBuy/frontend/src/components/ProductGrid/CardGrid.jsx
@@ -16,15 +16,34 @@ import CardProduct from './CardProduct';
 import myImage from '../../images/ip14.jpg'
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { maxHeight } from '@mui/system';
+import { useState, useEffect } from 'react'
+import { api } from '../../App';
+
 // import { Stack } from '@mui/system';
 
 function CardGrid(props) {
+  const [products, setproducts] = useState([])
+  const getProducts = () => {
+    api.get('/getallproducts').then(res => {
+      console.log(res.data.data);
+      setproducts(res.data.data);
+
+      // console.log(products);
+
+
+    })
+  }
     
+  useEffect(() => {
+    getProducts();
+  }, [])
+  
     return (
         <Stack direction="column" spacing={1} alignItems={"center"}>
         
         <Stack direction="row" spacing={1} alignItems={"center"}>
           {/* Image Not using props currently*/}
+          
           <CardProduct image={myImage} />
           <CardProduct image={myImage} />
           <CardProduct image={myImage} />
