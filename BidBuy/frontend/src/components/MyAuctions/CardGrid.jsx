1afa558131570d7104deaf29eba4de1286db039a
ADMANI <ahmedriaz-50@hotmail.com>
2022-11-30 18:42:36 +0500

    c

--- BidBuy/frontend/src/components/MyAuctions/CardGrid.jsx
+++ BidBuy/frontend/src/components/MyAuctions/CardGrid.jsx
@@ -26,18 +26,50 @@ import { api } from '../../App';
 
 function CardGrid(props) {
   const [products, setproducts] = useState([])
+  let double = [];
+
   const getProducts = () => {
     api.get('/getallproducts').then(res => {
-      console.log(res.data.data);
+      //console.log(res.data.data);
       setproducts(res.data.data);
+
       // console.log(products);
+
 
     })
   }
+  // const [filteredProducts, setFillteredProducts] = useState([]);
 
+
+  // const handleChange = (individualSpan) => {
+  //   console.log("Filter")
+  //   console.log(filteredProducts)
+  //   filterFunction(localStorage.getItem('userID'));
+  // }
+
+  const [filteredProducts, setFillteredProducts] = useState([]);
+
+  const filterFunction = (userId) => {
+    console.log("Hello")
+    console.log(products)
+    // console.log(products.category)
+    // console.log("Text " + text)
+    // console.log(products.category === text)
+    // console.log(products.filter((products) => products.id === localStorage.getItem('userID')))
+    const filter = products.filter((products) => products.user_id === userId);
+    setFillteredProducts(filter);
+
+    console.log('sdafa' + filteredProducts)
+  }
+
+
+
+  // filterFunction(localStorage.getItem('userID'));
+  const currentID = localStorage.getItem('userID') 
   useEffect(() => {
-    getProducts();
-  }, [])
+    getProducts()
+    filterFunction(currentID)
+  }, [filteredProducts],);
 
   
   return (
@@ -46,7 +78,7 @@ function CardGrid(props) {
 
         <Stack direction="row" spacing={1} alignItems={"center"}>
         <ImageList sx={{padding: '50px'}} cols={4} rowHeight={164}>
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
 
           <CardProduct product={product} />
 
---
+++ BidBuy/frontend/src/components/MyAuctions/CardProduct.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import {
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    Typography,
+    Button,
+    CardMedia,
+    IconButton,
+    Stack,
+    styled,
+    Fab
+
+} from '@mui/material';
+import myImage from '../../images/ip14.jpg';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { maxHeight } from '@mui/system';
+// import { Stack } from '@mui/system';
+
+function CardProduct(props) {
+    console.log(props.product.name);
+    const Img = styled('img')({
+        alignItems: "center",
+        maxwidth: "100%",
+        height: 150,
+        padding: 0,
+        marginLeft: '20%'
+        // paddingLeft: "25%"
+
+    });
+    return (
+        <Card elevation={0} sx={{boxShadow: '0 8px 24px 0 rgba(0,0,0,0.15)', padding: 1, borderRadius: 1, border: "1px solid #e1e1e1", backgroundColor: '#FEFEFd', maxWidth: 250, backgroundColor: 'white', maxHeight: 240, width: 250 }}>
+            {/* <Img  src = {myImage}/> */}
+            {/* <CardMedia
+                component="img"
+                height="150px"
+                width="100px"
+                // image={props.image}
+                image={myImage}
+            /> */}
+            <CardMedia sx = {{position: 'relative'}}>
+                <FavoriteIcon sx = {{position: 'absolute', marginLeft: '90%', color: '#CF3D2F'}} />
+                <Img src={props.product.image}  >
+
+                </Img>
+
+
+            </CardMedia>
+            <CardContent>
+
+
+                {/* <FavoriteIcon style={{ color: 'red' }} /> */}
+                <Typography variant="body2" color="black" sx={{ '&. MuiTypography-root': { color: 'black', padding: 2 } }} fontSize={14}>
+                    {props.product.name}
+                </Typography>
+
+                <Stack direction="row" spacing={2} justifyContent='space-between' alignItems={"center"}>
+                    <Typography variant="body2" color="black" fontWeight={"bold"} fontSize={14}>
+                    {props.product.price}
+                    </Typography>
+                    <Typography variant="body2" color="black" fontSize={12}>
+                        3 Bids
+                    </Typography>
+                </Stack>
+                <Stack direction="row" spacing={2} justifyContent='space-between' alignItems={"center"}>
+                    <Typography variant="body2" color="black" fontSize={12}>
+                    {props.product.description}
+                    </Typography>
+                    <Typography variant="body2" color="black" fontSize={12}>
+                        2d 3h
+                    </Typography>
+                </Stack>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default CardProduct
