4c2eb04d7e40829c43944eb6467b8f3156da6f58
ADMANI <ahmedriaz-50@hotmail.com>
2022-12-02 04:10:09 +0500

    Favorites done

--- BidBuy/frontend/src/components/MyFavorites/MyFavorites.jsx
+++ BidBuy/frontend/src/components/MyFavorites/MyFavorites.jsx
@@ -3,41 +3,14 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { api } from '../../App.jsx'
 import NavBar from '../NavBar/NavBar'
-import CardGrid from '../ProductGrid/CardGrid.jsx'
+import CardGrid from './CardGrid.jsx'
 import Heading from '../Text/Heading.jsx'
 import myImage from '../../images/ip14.jpg'
 import { Stack } from '@mui/system'
 
+
 function MyFavorites() {
-  const [products, setproducts] = useState([])
-  let double = [];
-  const getProducts = () => {
-    api.get('/getallproducts').then(res => {
-      //console.log(res.data.data);
-      setproducts(res.data.data);
-
-      // console.log(products);
-
-
-    })
-  }
-  const postProducts = () => {
-    api.post('/login', {
-      username: "MuhammadOsamaAsif9844",
-      password: "osama123456"
-    })
-      .then(function (response) {
-        console.log(response.data.data.token);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  // useEffect(() => {
-  //  getProducts()
-  // }, [])
-
+ 
   return (
     <div>
       <NavBar />
